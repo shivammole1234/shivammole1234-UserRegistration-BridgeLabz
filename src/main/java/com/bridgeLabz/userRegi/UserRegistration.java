8f0e3a7781ed722c97a191da2ff505487ef0b271
Shivam Mole <shivammole1713@gmail.com>
2024-09-18 23:42:10 +0530

    Commiting for use case 2

--- src/main/java/com/bridgeLabz/userRegi/UserRegistration.java
+++ src/main/java/com/bridgeLabz/userRegi/UserRegistration.java
@@ -16,11 +16,27 @@ public class UserRegistration {
         return matcher.matches();
     }
 
+    public boolean validate_last_name(String last_name){
+        String last_name_regex="^[A-Z][a-zA-Z]{2,}$";
+
+        Pattern pattern =Pattern.compile(last_name_regex);
+
+        if(last_name == null)
+            return false;
+        Matcher matcher = pattern.matcher(last_name);
+
+        return matcher.matches();
+    }
+
+
     public static void main(String[] args) {
 
         UserRegistration user_regi=new UserRegistration();
         System.out.println("is Shivam valid :- "+user_regi.validate_first_name("shiva"));
         System.out.println("is Shivam valid :- "+user_regi.validate_first_name("Shivam"));
+        System.out.println("is Mole valid :- "+user_regi.validate_last_name("Mole"));
+        System.out.println("is mole valid :- "+user_regi.validate_last_name("mole"));
+        System.out.println("is mo valid :- "+user_regi.validate_last_name("mo"));
 
     }
 }
--- src/test/java/com/bridgeLabz/userRegi/UserRegistrationTest.java
+++ src/test/java/com/bridgeLabz/userRegi/UserRegistrationTest.java
@@ -28,15 +28,46 @@ public class UserRegistrationTest extends TestCase {
     }
 
     @Test
+    public void test_when_valid_lname_should_return_True(){
+        boolean A_result=user_regi.validate_first_name("Mole");
+        assertTrue(A_result);
+        assertTrue(user_regi.validate_first_name("Smith"));
+        assertTrue(user_regi.validate_first_name("Xender"));
+        assertTrue(user_regi.validate_first_name("Tiaa"));
+        assertTrue(user_regi.validate_first_name("Julie"));
+
+    }
+
+    @Test
+    public void test_when_invalid_lname_should_return_False(){
+        assertFalse(user_regi.validate_first_name("mole"));
+        assertFalse(user_regi.validate_first_name("xeder"));
+        assertFalse(user_regi.validate_first_name("bill"));
+        assertFalse(user_regi.validate_first_name("gate"));
+    }
+
+    @Test
     public void test_null_first_name_return_false(){
         assertFalse(user_regi.validate_first_name(null));
 
     }
 
-    public void test_too_Short_fname_return_false(){
-        assertFalse(user_regi.validate_first_name("Ki"));
-        assertFalse(user_regi.validate_first_name("ki"));
+    public void test_too_short_fname_return_false(){
+        assertFalse(user_regi.validate_first_name("Xe"));
+        assertFalse(user_regi.validate_first_name("Mo"));
     }
+
+    @Test
+    public void test_null_l_name_return_false(){
+        assertFalse(user_regi.validate_first_name(null));
+
+    }
+
+    public void test_too_Short_lname_return_false(){
+        assertFalse(user_regi.validate_first_name("Xe"));
+        assertFalse(user_regi.validate_first_name("Mo"));
+    }
+
 
 
 }
